41cbc07e2718799ba88c2fca76ce41de9814c364
Pancho <pancholopez@gmail.com>
2023-03-04 13:27:40 +0100

    fixed console log template and webapi project path

---
+++ src/CleanArchitecture.WebApi/Program.cs
@@ -0,0 +1,61 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure()
+        .AddPresentation();
+
+    builder.Host
+        .UseSerilog((context, configuration)
+            => configuration.ReadFrom.Configuration(context.Configuration));
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseSerilogRequestLogging();
+
+    app.UseHttpsRedirection();
+
+    app.MapGet("/alive", (ILogger<Program> logger) =>
+        {
+            logger.LogInformation("Some info here!");
+            logger.LogWarning("This is a warning!");
+            logger.LogError(new Exception("error"), "This is an error!!!");
+            return "I am alive!";
+        })
+        .WithName("alive")
+        .WithOpenApi();
+
+    app.Run();
+}
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException")
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
+// Make it visible to tests projects
+public partial class Program
+{
+}
---
+++ src/CleanArchitecture.WebApi/Usings.cs
@@ -0,0 +1,5 @@
+﻿// Global using directives
+
+global using CleanArchitecture.Application;
+global using CleanArchitecture.Infrastructure;
+global using CleanArchitecture.Presentation;

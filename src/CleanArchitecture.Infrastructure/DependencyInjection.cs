e09b722319b2aa4fc09b1f8106bdc01dcecca48b
Pancho <pancholopez@gmail.com>
2023-03-04 10:37:35 +0100

    setup infrastructure project and updated nuget dependencies

---
+++ src/CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
